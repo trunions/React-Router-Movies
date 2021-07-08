e4dbb2b1f9a6bc948177c6df8e7691209bd31e39
Trista Runions <t.runions19@gmail.com>
2021-07-07 23:27:45 -0700

    finished project

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useHistory, Link } from 'react-router-dom';
 
 export default function MovieList(props) {
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
+        <Link key={movie.id} to={`/movies/${movie.id}`} >
         <MovieDetails key={movie.id} movie={movie} />
+      </Link>
       ))}
     </div>
   );
@@ -12,9 +15,10 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
+  const history = useHistory()
 
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={e => history.push(`/movies/${props.movie.id}`)}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
